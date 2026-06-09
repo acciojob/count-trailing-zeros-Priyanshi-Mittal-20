d4f74c207bbe48c583edb0c2b2f16e62e7c09e6c
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-09 18:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	if(n==0)
+	{
+		return 0
+	}
+	let fact=1
+	for(let i=1;i<=n;i++)
+		{
+			fact*=i
+		}
+	let arr=fact.split("")
+	let count=0
+	for(let j=arr.length-1;j>=0;j--)
+		{
+			if(arr[j]==0)
+			{
+				count++
+			}
+		}
+	return count
 }
 
 const input = prompt("Enter a number");
